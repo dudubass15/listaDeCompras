8529f96c9aeccbf6bd3d1baf4b8940ce312be188
Cadu <carlos.contato00@gmail.com>
2020-11-17 19:01:01 -0300

    Feito algumas melhorias na tela inicial

--- assets/js/custom/filter.js
+++ assets/js/custom/filter.js
@@ -2,6 +2,5 @@ let btnFilter = document.querySelector('#btnFilter');
 let containerFilter = document.querySelector('#filter');
 
 btnFilter.addEventListener('click', () => {
-    console.log('Clicou!');
     containerFilter.classList.toggle('active-filter');
 });
